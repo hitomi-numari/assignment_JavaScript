75f38a18bf3b49623ec446f557fea3e721920083
hitomi numari <h.h.hitomi1216@gmail.com>
2019-11-16 13:55:10 +0900

    OK

--- sample.js
+++ sample.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   function score_indicate(){
-    // このような記述をすることで、subject_pointsという変数の中に
-    // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]という配列を作成できる。
+  
     let subject_points = [Number($('#national_language').val()),
                           Number($('#english').val()),
                           Number($('#mathematics').val()),
@@ -20,10 +19,12 @@ $(document).ready(function(){
     // ここに、上記を参考にして平均点を出力する処理を書き込む
     let average = sum/subject_points.length;
     $("#average_indicate").text(average);
+    return average;
   };
 
-  function get_achievement(average){
+  function get_achievement(){
     // ここに、ランクの値の文字列（平均点が80点以上なら"A"、60点以上なら"B"、40点以上なら"C"、それ以下なら"D"）を出力する処理を書き込む
+    let average =  score_indicate();
     switch (true) {
       case (average >= 80):
         return "A";
@@ -36,8 +37,15 @@ $(document).ready(function(){
     }
   }
 
-  function get_pass_or_failure(subject_points){
+  function get_pass_or_failure(){
     // ここに、全ての教科が60点以上なら"合格"の文字列、一つでも60点未満の教科があったら"不合格"の文字列を出す処理を書き込む
+    let subject_points = [Number($('#national_language').val()),
+                          Number($('#english').val()),
+                          Number($('#mathematics').val()),
+                          Number($('#science').val()),
+                          Number($('#society').val())
+                          ];
+
     let number = subject_points.length;
     let judge = "合格";
     for (let i = 0; i < number; i++){
@@ -49,10 +57,10 @@ $(document).ready(function(){
     return judge;
   }
 
-  function judgement(subject_points){
+  function judgement(){
     // ここに、「最終ジャッジ」のボタンを押したら「あなたの成績はAです。合格です」といった内容を出力する処理を書き込む
-    let achievement = get_achievement(average);
-    let pass_or_failure =  get_pass_or_failure(subject_points);
+    let achievement = get_achievement();
+    let pass_or_failure =  get_pass_or_failure();
     // 下記の記述をすることで、「最終ジャッジ」のボタンを押すと「あなたの成績は（ここに「ランク」の値を入れる）です。（ここに「判定」の値を入れる）です」という文字の入った水色のフキダシが出力される処理が実装される。
     $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${achievement}です。${pass_or_failure}です</label>`);
   };
@@ -62,14 +70,16 @@ $(document).ready(function(){
   });
 
   $('#btn-evaluation').on('click',function() {
-    $('#evaluation').text(get_achievement(average));
+    let rank = get_achievement();
+    $('#evaluation').text(rank);
   });
 
   $('#btn-judge').on('click',function() {
-    get_pass_or_failure();
+    let judge = get_pass_or_failure();
+    $('#judge').text(judge);
   });
 
   $('#btn-declaration').on('click',function() {
-    judgement();
+    let final_judge = judgement();
   });
 });
